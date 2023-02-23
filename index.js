9885498e73fad4e8158dd34b3b2315bd274df1b3
Tomiarag <taragusuku96@gmail.com>
2023-02-23 14:07:15 -0300

    inicializado de proyecto

---
+++ index.js
@@ -0,0 +1,12 @@
+
+
+function onClickMenu(menuItem) {
+
+    // buscar todos los elementos que tengan la clase  "bokita"
+    // iterar x los elementos de la lista
+    // eliminar clase bokita del os elementos que la tengan asignada
+
+
+    var element = document.getElementById(menuItem)
+    element.classList.add("bokita")
+}
