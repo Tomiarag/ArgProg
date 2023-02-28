f64940e47dc83417fe2eba4c217fea35f221d947
Tomiarag <117741059+Tomiarag@users.noreply.github.com>
2023-02-27 23:03:50 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
-
+//función para resaltar los items deseados clickeando en los botones de la barra de navegación 
 
 function onClickMenu(menuItem) {
-
-    // buscar todos los elementos que tengan la clase  "bokita"
-    // iterar x los elementos de la lista
-    // eliminar clase bokita del os elementos que la tengan asignada
-
-
     var element = document.getElementById(menuItem)
-    element.classList.add("bokita")
+    element.classList.toggle("bokita")
 }
